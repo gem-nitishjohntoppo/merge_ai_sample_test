820ff6d4ecbb291ccedcf91f7ebad44c0616ad63
gem-nitishjohntoppo <nitish.toppo@geminisolutions.com>
2025-02-24 16:33:41 +0530

    feature file2

---
+++ file2.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name):
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+person = Person("Bob")
+print("Name:", person.name)
