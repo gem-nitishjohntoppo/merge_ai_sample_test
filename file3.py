a735e00241d37ea4d9b3cd8833a8cced75521afa
gem-nitishjohntoppo <nitish.toppo@geminisolutions.com>
2025-02-25 14:34:35 +0530

    file3.py

---
+++ file3.py
@@ -0,0 +1,9 @@
+def find_duplicates_optimized(lst):
+    seen = set()
+    duplicates = set()
+    for num in lst:  
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return list(duplicates)
