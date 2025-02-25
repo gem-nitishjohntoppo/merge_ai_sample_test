a61062a2ed98bdf646df3174eb450680402ec2d4
gem-nitishjohntoppo <nitish.toppo@geminisolutions.com>
2025-02-25 14:37:05 +0530

    file3.py

---
+++ file3.py
@@ -0,0 +1,7 @@
+def find_duplicates(lst):
+    duplicates = []
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            if lst[i] == lst[j] and lst[i] not in duplicates:
+                duplicates.append(lst[i])
+    return duplicates
