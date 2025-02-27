02bbcdc753fe64da09197827c141402fb5ef0595
gem-nitishjohntoppo <nitish.toppo@geminisolutions.com>
2025-02-27 12:20:57 +0530

    testing webhooks

--- file4.py
+++ file4.py
@@ -1,5 +1,5 @@
 def generate_squares(n):
     squares = []
     for i in range(n):
-        squares.append(i * i) #Testing webhook2
+        squares.append(i * i) #Testing webhook3
     return squares
